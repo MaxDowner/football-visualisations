c56b03aa8ec43e138d8774a07d8d7619f8a528ca
Max <maxdowner96@gmail.com>
2024-11-16 21:50:31 +0000

    fixed headers and assigned relevant series to variables

--- scrape.py
+++ scrape.py
@@ -8,29 +8,38 @@ import numpy as np
 # use the read_html function to read the table into a list
 # store the 0th element of the output list into a dataframe
 # print the data frame
-# advanced_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepersadv/players/2023-2024-Big-5-European-Leagues-Stats"
-# advanced_stats_list = pd.read_html(advanced_stats_url)
-# advanced_stats_df = advanced_stats_list[0]
+advanced_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepersadv/players/2023-2024-Big-5-European-Leagues-Stats"
+advanced_stats_list = pd.read_html(advanced_stats_url)
+advanced_stats_df = advanced_stats_list[0]
 # print(advanced_stats_df)
 # print(type(advanced_stats_df))
 
 # as above but for basic goalkeeping stats rather than advanced stats
-# basic_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepers/players/2023-2024-Big-5-European-Leagues-Stats"
-# basic_stats_list = pd.read_html(basic_stats_url)
-# basic_stats_df = basic_stats_list[0]
+basic_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepers/players/2023-2024-Big-5-European-Leagues-Stats"
+basic_stats_list = pd.read_html(basic_stats_url)
+basic_stats_df = basic_stats_list[0]
 # print(basic_stats_df)
 
 # repeated "Unnamed X_level_0", I think due to how the headers are structured
 # link below shows how to fix that
 # https://www.youtube.com/watch?v=I0TB9m_hyCo
 
+advanced_stats_df.columns = ['_'.join(col) for col in advanced_stats_df.columns.values]
+basic_stats_df.columns = ['_'.join(col) for col in basic_stats_df.columns.values]
+# print(advanced_stats_df.columns)
+# print(basic_stats_df.columns)
+# ok it's kind of worked
+
 # got confused with read_html creating a list of dataframes rather than a dataframe
 # print(type()) confirms we have a dataframe now
 
 # next jobs are
 #   access the PSxg-GA column (series?) in advanced_stats_df and assign to a variable
+# print(advanced_stats_df.loc[:,"Expected_PSxG/SoT"]) confirmed the correct data
+psxg = advanced_stats_df.loc[:,"Expected_PSxG"]
 #   access the shots faced column (series) in basic_stats_df and assign to a variable
-
+shots_faced = basic_stats_df.loc[:,"Performance_SoTA"]
+print(shots_faced)
 
 # plot PSxG-GA against shots faced using matplotlib.pyploy
 
