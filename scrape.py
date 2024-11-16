66c8ce6fc7fd832f2fec1d26136c5c1b23e257aa
Max <maxdowner96@gmail.com>
2024-11-16 19:59:41 +0000

    added the tiniest bit of matplotlib

--- scrape.py
+++ scrape.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 # store the url of the webpage containing the desired table in a variable
 # use the read_html function to read the table into a list
 # store the 0th element of the output list into a dataframe
 # print the data frame
-advanced_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepersadv/players/2023-2024-Big-5-European-Leagues-Stats"
-advanced_stats_list = pd.read_html(advanced_stats_url)
-advanced_stats_df = advanced_stats_list[0]
+# advanced_stats_url = "https://fbref.com/en/comps/Big5/2023-2024/keepersadv/players/2023-2024-Big-5-European-Leagues-Stats"
+# advanced_stats_list = pd.read_html(advanced_stats_url)
+# advanced_stats_df = advanced_stats_list[0]
 # print(advanced_stats_df)
 # print(type(advanced_stats_df))
 
@@ -24,11 +28,23 @@ advanced_stats_df = advanced_stats_list[0]
 # next jobs are
 #   access the PSxg-GA column (series?) in advanced_stats_df
 #   access the shots faced column (series) in basic_stats_df
-# ('Expected', 'PSxG+/-')
+# series are nested ('Expected', 'PSxG+/-')
 
 # print(advanced_stats_df.columns.values)
 
 
 # plot PSxG-GA against shots faced using matplotlib.pyploy
 
+# maybe hack around  for now, bypassing pandas and going from sheets > matplotlib
+
+# Plotting
+
+# Setting up the figure
+
+# initialising fig and ax
+fig, ax = plt.subplots()
+# plot
+# ax.scatter(x, y)
+
+# https://matplotlib.org/stable/tutorials/lifecycle.html
 
